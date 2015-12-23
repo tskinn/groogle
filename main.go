471c24b780d7f4d13db566a334cda0e147080b36
Taylor Skinner <tskinner@kyanicorp.com>
2015-12-23 11:42:58 -0700

    baby steps

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 //	"encoding/json"
 )
 
@@ -33,4 +34,31 @@ func main() {
 	for _, link := range search.Items {
 		fmt.Println(link.Link)
 	}
+
+	// go get links and stuff and search them
+	
+}
+
+// fetch the actual page. maybe only get the body of html? maybe not
+func getLink() string {
+	return ""
+}
+
+// get all the references to the key string
+func getReferences(page, key string) []string {
+	count := strings.Count(page, key)
+	references := make([]string, count)
+	rest := page
+	reference := ""
+	for i := 0; i < count; i++ {
+		rest, reference = getReference(rest, key)
+		references = append(references, reference)
+	}
+	return references
+}
+
+// find single reference to key and get surounding context and rest of body to search
+func getReference(rest, key string) (string, string) {
+	index := strings.Index(rest, key)
+	return rest[index:], rest[index-10:index+10]
 }
